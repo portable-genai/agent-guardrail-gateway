portable-genai/agent-observability

Make customer-managed encryption optional, and off by default
Every binding to the stack's Cloud KMS key now sits behind cmek_enabled,
default false: the key ring, the key, every grant to a service agent, and
every store, log bucket, registry and revision that took the key. Block
bindings became dynamic blocks; scalar bindings read the key through one(),
which is null when the key does not exist and omits the attribute rather
than pointing it at nothing. The Terraform tests that assert on key material
turn the gate on to keep meaning what they meant.

Why the default is off: a key ring can never be deleted, a log bucket that
has CMEK can never drop it, and registries and document stores take their
key at creation. None of that changes an answer or a screen, and everything
is encrypted at rest with Google-managed keys regardless. A deployment with
a customer whose data it must shred, whose key access must be audited or
whose keys must live in an HSM sets it true in its own tfvars BEFORE the
first apply. Where a stack already applied it, the key's prevent_destroy
refuses a plan that omits the line, which is the right answer: the stores it
bound stay bound.

The vendored deploy-and-residency-hardening skill says the same.

infra/terraform/logging_worm.tf
@@ -54,8 +54,11 @@ resource "google_logging_project_bucket_config" "worm_audit" {
   # is encrypted with a Google-managed key and the "bank controls the audit key" claim in
   # COMPLIANCE.md is not true. Destroying the key crypto-shreds the trail, which is the
   # only lever that exists once `locked = true` forbids deletion.
-  cmek_settings {
-    kms_key_name = google_kms_crypto_key.audit.id
+  dynamic "cmek_settings" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      kms_key_name = one(google_kms_crypto_key.audit[*].id)
+    }
   }
 
   depends_on = [

portable-genai/agent-guardrail-gateway

infra/terraform/logging_worm.tf
@@ -28,8 +28,11 @@ resource "google_storage_bucket" "audit_worm" {
     enabled = true
   }
 
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.guardrail.id
+  dynamic "encryption" {
+    for_each = var.cmek_enabled ? [1] : []
+    content {
+      default_kms_key_name = one(google_kms_crypto_key.guardrail[*].id)
+    }
   }
 
   depends_on = [
@@ -40,7 +43,8 @@ resource "google_storage_bucket" "audit_worm" {
 
 # The storage service agent must be able to use the CMEK key, or bucket creation fails.
 resource "google_kms_crypto_key_iam_member" "storage_cmek" {
-  crypto_key_id = google_kms_crypto_key.guardrail.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.guardrail[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gs-project-accounts.iam.gserviceaccount.com"
 }